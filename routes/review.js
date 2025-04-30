935d8efbad99858e3e49e3ee0cc8415210c5bf9b
khushisahu476 <146587867+khushisahu476@users.noreply.github.com>
2025-04-30 17:30:46 +0530

    final push

---
+++ routes/review.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const Review = require("../models/review.js");
+const Listing = require('../models/listing.js');
+const {validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
+
+//delete reviews route
+router.delete("/:reviewId",
+    isLoggedIn,
+    isReviewAuthor,
+    wrapAsync(reviewController.destroyReview)
+);
+
+//post route
+router.post(
+    "/",
+    isLoggedIn,
+    validateReview,
+    wrapAsync(reviewController.newReview)
+);
+
+module.exports = router;
